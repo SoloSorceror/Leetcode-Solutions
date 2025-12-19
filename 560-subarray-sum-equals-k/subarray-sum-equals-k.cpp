ba99a5cf12b2f90762b9d7f41f8557eb0e99b348
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2025-12-19 12:28:14 +0530

    Time: 35 ms (88.32%) | Memory: 45.5 MB (40.50%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -3,17 +3,17 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int,int> map;
         map[0] = 1;
-        int total_sum = 0 , count = 0;
+        long long sum = 0;
+        int count = 0;
 
-        for(int i =0; i<nums.size(); i++){
-            total_sum += nums[i];
-            int x = total_sum - k;
-            if(map.find(x) != map.end()){
-                count += map[x];
+        for(auto &a: nums){
+            sum +=a;
+            int target = sum - k;
+            if(map.find(target) != map.end()){
+                count += map[target];
             }
-            map[total_sum] ++;
+            map[sum] ++;
         }
         return count;
-
     }
 };
