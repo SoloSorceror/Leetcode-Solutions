456cf5e416fac4d9edf1e2de60ea46d5b9e69e39
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2025-12-27 18:34:37 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (73.75%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,19 +3,18 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        queue<pair<int,int>> q;
         int fresh = 0;
-        int time =0;
-        int delRow[4] = {-1, 0, 1, 0};
-        int delCol[4] = {0, 1, 0, -1};
+        int time = 0;
+
+        queue<pair<int,int>> q;
 
         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j] == 1){
-                    fresh++;
-                }
-                if(grid[i][j]==2){
+            for(int j =0; j<n; j++){
+                if(grid[i][j] == 2){
                     q.push({i,j});
+                }
+                else if(grid[i][j]==1){
+                    fresh++;
                 }
             }
         }
@@ -24,22 +23,26 @@ public:
             int size = q.size();
             time++;
 
-            for(int i=0; i<size;i++){
+            int dr[4] = {-1,0,1,0};
+            int dc[4] = {0,1,0,-1};
+
+            for(int i=0; i<size; i++){
                 auto [r,c] = q.front();
                 q.pop();
 
                 for(int k =0; k<4; k++){
-                    int nr = r + delRow[k];
-                    int nc = c + delCol[k];
+                    int nr = r + dr[k];
+                    int nc = c + dc[k];
 
-                    if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc] ==1){
+                    if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc] == 1){
                         grid[nr][nc] = 2;
                         q.push({nr,nc});
                         fresh--;
                     }
-                }
+                } 
             }
         }
-        return fresh == 0 ? time : -1;
+        return fresh==0 ? time : -1;
+
     }
 };
