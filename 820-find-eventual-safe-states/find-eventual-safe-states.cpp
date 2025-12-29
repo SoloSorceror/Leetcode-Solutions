b3c8c27f15823e788401dfb37bfea59a4ae09885
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2025-12-29 20:02:34 +0530

    Time: 3 ms (94.60%) | Memory: 53.1 MB (65.70%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool dfs(int node, vector<vector<int>>& graph, vector<int>& visited, vector<int>& path, vector<int>& safe) {
+        visited[node] = 1;
+        path[node] = 1;
+
+        for (auto &nbr : graph[node]) {
+            if (!visited[nbr]) {
+                if (dfs(nbr, graph, visited, path, safe))
+                    return true; // cycle found
+            }
+            else if (path[nbr]) {
+                return true; // back edge → cycle
+            }
+        }
+
+        path[node] = 0;
+        safe[node] = 1; // no cycle from here
+        return false;
+    }
+
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int V = graph.size();
+        vector<int> visited(V, 0), path(V, 0), safe(V, 0);
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                dfs(i, graph, visited, path, safe);
+            }
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < V; i++) {
+            if (safe[i])
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
