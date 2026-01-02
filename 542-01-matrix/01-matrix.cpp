4e3f6aa783ca766ba2a5fb05f1e1e043c3e491a3
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-01-02 21:47:35 +0530

    Time: 16 ms (66.73%) | Memory: 34.6 MB (92.72%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -4,15 +4,14 @@ public:
         int m = mat.size();
         int n = mat[0].size();
 
-        vector<vector<int>> distance(m, vector<int>(n, 0));
-        vector<vector<int>> visited(m, vector<int>(n, 0));
-        queue<pair<pair<int, int>, int>> q;
+        vector<vector<int>> distance(m, vector<int>(n, 1e9));
+        queue<pair<int, int>> q;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 0) {
-                    q.push({{i, j}, 0});
-                    visited[i][j] =1;
+                    q.push({i, j});
+                    distance[i][j] =0;
                 }
             }
         }
@@ -21,19 +20,17 @@ public:
         int delCol[4] = {0, 1, 0, -1};
 
         while (!q.empty()) {
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int dist = q.front().second;
+            int r = q.front().first;
+            int c = q.front().second;
             q.pop();
-            distance[r][c] = dist;
 
             for (int i = 0; i < 4; i++) {
                 int nr = r + delRow[i];
                 int nc = c + delCol[i];
 
-                if(nr>=0 && nr<m && nc>=0 && nc<n && !visited[nr][nc]){
-                    q.push({{nr,nc},dist+1});
-                    visited[nr][nc] =1;
+                if(nr>=0 && nr<m && nc>=0 && nc<n && distance[r][c] + 1< distance[nr][nc]){
+                    q.push({nr,nc});
+                    distance[nr][nc] = distance[r][c] + 1;
                 }
             }
         }
