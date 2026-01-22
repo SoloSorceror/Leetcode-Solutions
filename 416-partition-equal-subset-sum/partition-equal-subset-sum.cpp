af04d00f21c5cbb88da77d8e990f7ff4743ecc6c
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-01-22 22:34:43 +0530

    Time: 199 ms (11.95%) | Memory: 17.4 MB (65.55%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        if(sum%2 !=0) return false;
+        int target = sum/2;
         int n = nums.size();
-        int sum = accumulate(nums.begin(), nums.end(), 0);
 
-        if (sum % 2 != 0) return false;
+        vector<vector<bool>> dp(n+1,vector<bool>(target+1,false));
+        dp[0][0] = 1;
 
-        int target = sum / 2;
-
-        vector<vector<bool>> dp(n+1, vector<bool>(target+1, false));
-
-        dp[0][0] = true;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= target; j++) {
-
-                // exclude
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<=target; j++){
                 dp[i][j] = dp[i-1][j];
 
-                // include
-                if (j >= nums[i-1]) {
-                    dp[i][j] = dp[i][j] || dp[i-1][j - nums[i-1]];
+                if(j>=nums[i-1]){
+                    dp[i][j] = dp[i][j] || dp[i-1][j-nums[i-1]];
                 }
             }
         }
-
         return dp[n][target];
     }
 };
