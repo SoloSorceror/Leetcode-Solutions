ca53347bed4ca5bbdb08784d50b9707675b18edc
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-01-30 10:36:17 +0530

    Time: 494 ms (23.44%) | Memory: 59.6 MB (82.45%) - LeetSync

--- 833-bus-routes/bus-routes.cpp
+++ 833-bus-routes/bus-routes.cpp
@@ -1,43 +1,36 @@
 class Solution {
 public:
-    int numBusesToDestination(vector<vector<int>>& routes, int source,
-                              int target) {
-        if (source == target)
-            return 0;
-        unordered_map<int, vector<int>> stopToRoutes;
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        if(source == target) return 0;
+        unordered_map<int,vector<int>> map;
         int n = routes.size();
-
-        // Build stop -> routes map
-        for (int i = 0; i < n; i++) {
-            for (int stop : routes[i]) {
-                stopToRoutes[stop].push_back(i);
+        for(int i=0; i<n; i++){
+            for(int route: routes[i]){
+                map[route].push_back(i);
             }
         }
+        queue<int> q;
+        unordered_set<int> visited;
 
-        queue<int> q; // store route index
-        unordered_set<int> visitedRoutes;
-
-        // Start with all routes that include source
-        for (int route : stopToRoutes[source]) {
+        for(int route: map[source]){
             q.push(route);
-            visitedRoutes.insert(route);
+            visited.insert(route);
         }
 
         int buses = 1;
-        while (!q.empty()) {
+        while(!q.empty()){
             int size = q.size();
-            while (size--) {
-                int currRoute = q.front();
+            while(size--){
+                int curr = q.front();
                 q.pop();
 
-                // check all stops in this route
-                for (int stop : routes[currRoute]) {
-                    if (stop == target)
-                        return buses;
-                    for (int nextRoute : stopToRoutes[stop]) {
-                        if (!visitedRoutes.count(nextRoute)) {
-                            visitedRoutes.insert(nextRoute);
-                            q.push(nextRoute);
+                for(int stop: routes[curr]){
+                    if(stop == target) return buses;
+
+                    for(int next: map[stop]){
+                        if(!visited.count(next)){
+                            visited.insert(next);
+                            q.push(next);
                         }
                     }
                 }
