4f466d7b92b20755c065900d623cfed2f05c861c
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-02-04 19:42:02 +0530

    Time: 64 ms (13.83%) | Memory: 34.3 MB (11.90%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -4,12 +4,12 @@ public:
         unordered_map<string, vector<string>> map;
         for (auto& s : strs) {
             vector<int> count(26,0);
-            for(auto &x: s){
-                count[x - 'a']++;
+            for(auto &c: s){
+                count[c-'a']++;
             }
             string key = "";
             for(int a: count){
-                key += "," + to_string(a);
+                key += to_string(a) + ",";
             }
             map[key].push_back(s);
         }
