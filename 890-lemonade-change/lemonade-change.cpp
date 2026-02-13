914d6601668cc72fd19e3aef21edd7329e79e010
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-02-13 11:13:27 +0530

    Time: 0 ms (100.00%) | Memory: 87.1 MB (95.38%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        vector<int> count(3,0);
+        for(int i=0; i<bills.size(); i++){
+            int val = bills[i];
+            if(val == 5) count[0]++;
+            if(val == 10) count[1]++;
+            //if(val == 20) count[2]++;
+
+            int change = val - 5;
+            if(change == 0) continue;
+
+            // while(change >= 20 && count[2] > 0){
+            //     change -= 20;
+            //     count[2]--;
+            // }
+            while(change >= 10 && count[1] > 0){
+                change -= 10;
+                count[1]--;
+            }
+            while(change >= 5 && count[0] > 0){
+                change -= 5;
+                count[0]--;
+            }
+            if(change != 0) return false;
+        }
+        return true;
+    }
+};
