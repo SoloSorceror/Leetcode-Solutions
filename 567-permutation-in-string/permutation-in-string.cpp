595e6fa5371fa75fb0ec33a972aac2142e9c4853
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-04-22 18:34:11 +0530

    Time: 1 ms (72.97%) | Memory: 9.9 MB (69.53%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,47 +1,21 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if (s1.length() > s2.length()) {
-            return false;
-        }
+        if(s1.size()>s2.size()) return false;
+        int left = 0;
+        vector<int> freq1(26,0);
+        vector<int> freq2(26,0);
 
-        vector<int> s1Count(26, 0);
-        vector<int> s2Count(26, 0);
-        for (int i = 0; i < s1.length(); i++) {
-            s1Count[s1[i] - 'a']++;
-            s2Count[s2[i] - 'a']++;
-        }
+        for(char c: s1) freq1[c-'a']++;
 
-        int matches = 0;
-        for (int i = 0; i < 26; i++) {
-            if (s1Count[i] == s2Count[i]) {
-                matches++;
+        for(int right=0; right<s2.size(); right++){
+            freq2[s2[right]-'a']++;
+            while(right-left+1 > s1.size()){
+                freq2[s2[left]-'a']--;
+                left++;
             }
+            if(freq1==freq2) return true;
         }
-
-        int l = 0;
-        for (int r = s1.length(); r < s2.length(); r++) {
-            if (matches == 26) {
-                return true;
-            }
-
-            int index = s2[r] - 'a';
-            s2Count[index]++;
-            if (s1Count[index] == s2Count[index]) {
-                matches++;
-            } else if (s1Count[index] + 1 == s2Count[index]) {
-                matches--;
-            }
-
-            index = s2[l] - 'a';
-            s2Count[index]--;
-            if (s1Count[index] == s2Count[index]) {
-                matches++;
-            } else if (s1Count[index] - 1 == s2Count[index]) {
-                matches--;
-            }
-            l++;
-        }
-        return matches == 26;
+        return false;
     }
 };
