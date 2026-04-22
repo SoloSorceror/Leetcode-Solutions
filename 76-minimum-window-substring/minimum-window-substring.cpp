3ae65b1eb1eccdeba7e2527e7ac5cbf2b0aab64c
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-04-22 19:16:16 +0530

    Time: 1 ms (83.49%) | Memory: 11.5 MB (41.05%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         vector<int> freq(256,0);
+        int count = t.size();
+        int startIdx = 0;
+        int minWindow = INT_MAX;
+        int left = 0;
+
         for(char c: t) freq[c]++;
 
-        int left = 0, count = t.size();
-        int minLen = INT_MAX, startIdx = 0;
-
-        for(int right =0; right<s.size(); right++){
-            if(freq[s[right]] > 0) count--;
+        for(int right=0; right<s.size(); right++){
+            if(freq[s[right]]>0) count--;
             freq[s[right]]--;
 
-            while(count == 0){
-                if(right-left+1<minLen){
-                    minLen = right-left+1;
+            while(count==0 ){
+                if(right-left+1<minWindow){
                     startIdx = left;
+                    minWindow = right-left+1;
                 }
                 freq[s[left]]++;
                 if(freq[s[left]]>0) count++;
                 left++;
             }
         }
-        return minLen == INT_MAX? "": s.substr(startIdx,minLen);
+        return minWindow==INT_MAX ? "": s.substr(startIdx,minWindow);
     }
 };
