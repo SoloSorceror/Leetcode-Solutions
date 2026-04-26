7f8b4b01c5b2625d437b7715149b6d2c4e4f9ba2
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-04-26 13:45:06 +0530

    Time: 3 ms (78.14%) | Memory: 11.6 MB (24.19%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        if(t.size() > s.size()) return "";
         vector<int> freq(256,0);
-        int count = t.size();
-        int startIdx = 0;
-        int minWindow = INT_MAX;
+        int count = 0;
         int left = 0;
+        int minWindowSize = INT_MAX;
+        int startIdx = 0;
 
-        for(char c: t) freq[c]++;
+        for(char c: t){
+            freq[c]++;
+            count++;
+        }
 
         for(int right=0; right<s.size(); right++){
-            if(freq[s[right]]>0) count--;
+            if(freq[s[right]] > 0) count--;
             freq[s[right]]--;
 
-            while(count==0 ){
-                if(right-left+1<minWindow){
+            while(count == 0){
+                if(right-left+1<minWindowSize){
+                    minWindowSize = right-left+1;
                     startIdx = left;
-                    minWindow = right-left+1;
                 }
                 freq[s[left]]++;
-                if(freq[s[left]]>0) count++;
+                if(freq[s[left]] > 0) count++;
                 left++;
             }
         }
-        return minWindow==INT_MAX ? "": s.substr(startIdx,minWindow);
+        return minWindowSize == INT_MAX ? "" : s.substr(startIdx,minWindowSize);
     }
 };
