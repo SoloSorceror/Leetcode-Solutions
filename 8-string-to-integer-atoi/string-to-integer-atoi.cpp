9adc53a9ad4b19edb88ffa7c36b74cdbc0a8afdf
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-05-09 17:34:57 +0530

    Time: 4 ms (5.53%) | Memory: 10.4 MB (5.05%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
-    int myAtoi(string s) {
+    void rec(int index, long long &number, string s, int sign){
         int n = s.size();
+        if(index >= n) return;
+        if(isdigit(s[index])){
+            int digit = s[index] - '0';
+            if(number > (INT_MAX - digit)/10){
+                number = sign==1? INT_MAX:INT_MIN;
+                return;
+            }
+            number = number*10 + digit;
+            rec(index+1,number,s,sign);
+        }else return;
+    }
+    int myAtoi(string s) {
         int i = 0;
+        int n = s.size();
         int sign = 1;
         long long number = 0;
 
-        while(i<n && s[i] == ' '){
+        while(i<n && s[i]==' '){
             i++;
         }
-        if(i<n && s[i] == '-' || s[i]=='+'){
+
+        if(i<n && s[i]=='-' || s[i]=='+'){
             if(s[i]=='-') sign = -1;
             i++;
         }
-        while(i<n && isdigit(s[i])){
-            int digit = s[i] - '0';
-            if(number > (INT_MAX-digit)/10){
-                return sign==1? INT_MAX: INT_MIN;
-            }
-            number = number * 10 + digit;
-            i++;
-        }
+
+        rec(i,number,s,sign);
         return number*sign;
     }
 };
