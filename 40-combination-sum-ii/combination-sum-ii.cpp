1c90430fc7aae47dc09fdfca1222f92f6e5a0fef
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-05-11 21:01:05 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (33.14%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    void rec(int idx, vector<int>& nums, vector<int>& curr,
-             vector<vector<int>>& ans, int target) {
-        if (target == 0) {
+    void rec(int idx, vector<int>& nums,vector<int> &curr,vector<vector<int>> &ans, int target){
+        if(target==0){
             ans.push_back(curr);
             return;
         }
-        if (target < 0) {
-            return;
-        }
-        for (int i = idx; i < nums.size(); i++) {
-            if (i > idx && nums[i - 1] == nums[i])
-                continue;
+        for(int i=idx; i<nums.size(); i++){
+            if(i>idx && nums[i-1]==nums[i]) continue;
+            if(nums[i]>target) break;
             curr.push_back(nums[i]);
-            rec(i + 1, nums, curr, ans, target - nums[i]);
+            rec(i+1,nums,curr,ans,target-nums[i]);
             curr.pop_back();
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<int> curr;
         vector<vector<int>> ans;
-        sort(candidates.begin(), candidates.end());
-        rec(0, candidates, curr, ans, target);
+        sort(candidates.begin(),candidates.end());
+        rec(0,candidates,curr,ans,target);
         return ans;
     }
 };
