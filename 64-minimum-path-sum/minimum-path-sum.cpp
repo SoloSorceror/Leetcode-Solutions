f3aa04402b3c3941a0e59fcec1018f3b89f43412
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-05-20 17:47:01 +0530

    Time: 2 ms (33.42%) | Memory: 16.4 MB (14.25%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,47 +1,16 @@
 class Solution {
+private:
+    int rec(int row, int col, vector<vector<int>> &grid,vector<vector<int>> &dp){
+        if(row<0 || col <0) return 1e9;
+        if(row == 0 && col ==0) return grid[0][0];
+        if(dp[row][col] != -1) return dp[row][col];
+        return dp[row][col] =  grid[row][col] + min(rec(row-1,col,grid,dp),rec(row,col-1,grid,dp));
+    }
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-
-        priority_queue<
-            pair<int, pair<int,int>>,
-            vector<pair<int, pair<int,int>>>,
-            greater<>
-        > pq;
-
-        dist[0][0] = grid[0][0];
-        pq.push({grid[0][0], {0, 0}});
-
-        // ONLY right & down
-        int dr[2] = {0, 1};
-        int dc[2] = {1, 0};
-
-        while(!pq.empty()) {
-            auto [d, cell] = pq.top();
-            pq.pop();
-
-            int r = cell.first;
-            int c = cell.second;
-
-            if(d > dist[r][c]) continue;
-
-            for(int i = 0; i < 2; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-
-                if(nr < n && nc < m) {
-                    int nd = d + grid[nr][nc];
-                    if(nd < dist[nr][nc]) {
-                        dist[nr][nc] = nd;
-                        pq.push({nd, {nr, nc}});
-                    }
-                }
-            }
-        }
-
-        return dist[n-1][m-1];
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return rec(n-1, m-1, grid,dp);
     }
 };
