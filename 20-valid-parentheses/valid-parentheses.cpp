97a66c0c87865b55d263da1ae498e602229f37e8
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-06-13 22:07:02 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (37.32%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char c: s){
+            if(st.empty()){
+                st.push(c);
+                continue;
+            }
+            if(st.top() == '{' && c=='}' || st.top() == '(' && c==')' || st.top() == '[' && c==']'){
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+        return st.empty();
+    }
+};
