85d6eafbdcfe54e759df79d581d5b66bb5afd026
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-06-22 10:39:12 +0530

    Time: 7 ms (14.86%) | Memory: 8.7 MB (38.01%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x<0) return false;
-        long long initial = x;
-        long long newNo = 0;
+        int initial = x;
+        long  newNo = 0;
         while(x>0){
             newNo = newNo*10 + x%10;
+            if(newNo > INT_MAX) return false;
             x/=10;
         }
-        return newNo == initial;
+        return (int)newNo == initial;
     }
 };
