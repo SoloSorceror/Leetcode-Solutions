ab25d7158f81ed607756cd5da36aa0500dc652d1
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-06-30 00:23:00 +0530

    Time: 29 ms (94.10%) | Memory: 209.1 MB (95.25%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,30 +1,52 @@
-class Solution {
+class DSU{
 public:
-    bool dfs(int src, int dst, vector<vector<int>>& adj, vector<int>& visited) {
-        visited[src] = true;
-        if (src == dst) {
-            return true;
+    vector<int> parent, rank;
+
+    DSU(int n){
+        parent.resize(n);
+        rank.resize(n,0);
+
+        for(int i=0; i<n; i++){
+            parent[i] = i;
         }
-        for (auto& s : adj[src]) {
-            if (!visited[s]) {
-                if (dfs(s, dst, adj, visited)) {
-                    return true;
-                }
-            }
-        };
-        return false;
     }
 
-    bool validPath(int n, vector<vector<int>>& edges, int source,
-                   int destination) {
-        vector<vector<int>> adj(n);
-        vector<int> visited(n);
-
-        for (auto& e : edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
+    int findParent(int x){
+        if(parent[x] != x){
+            parent[x] = findParent(parent[x]);
         }
+        return parent[x];
+    }
 
-        return dfs(source, destination, adj, visited);
+    void unite(int u, int v){
+        int pou = findParent(u);
+        int pov = findParent(v);
+
+        if(rank[pou] > rank[pov]){
+            parent[pov] = pou;
+        }
+        else if(rank[pov] > rank[pou]){
+            parent[pou] = pov;
+        }
+        else{
+            parent[pov] = pou;
+            rank[pou]++;
+        }
+    }
+
+};
+
+
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        DSU dsu(n);
+        for(auto &e: edges){
+            int p1 = dsu.findParent(e[0]);
+            int p2 = dsu.findParent(e[1]);
+
+            if(p1 != p2) dsu.unite(p1,p2);
+        }
+        return dsu.findParent(source) == dsu.findParent(destination);
     }
 };
