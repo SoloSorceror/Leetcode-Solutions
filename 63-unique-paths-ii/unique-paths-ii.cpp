907513c5ec86355cc83b031536152ed7f800e8f6
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-07-01 11:45:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (85.19%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-
-    int rec(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& dp){
-        if(row == 0 && col == 0 && grid[row][col] != 1) return 1;
-        if(row <0 || col < 0 || grid[row][col] == 1) return 0;
-
-        if(dp[row][col] != -1) return dp[row][col];
-
-        int left = rec(row-1,col,grid,dp);
-        int right = rec(row,col-1,grid,dp);
-
-        return dp[row][col] = left + right;
-    }
-
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
 
-        vector<vector<int>> dp(n,vector<int>(m,-1));
+        vector<int> dp(m,0);
 
-        return rec(n-1,m-1,obstacleGrid,dp);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(obstacleGrid[i][j] == 1){
+                    dp[j] = 0;
+                }else if(i==0 && j==0){
+                    dp[j] = 1;
+                }
+                else if(j>0){
+                    dp[j] += dp[j-1];
+                }
+            }
+        }
+
+        return dp[m-1];
+
     }
 };
