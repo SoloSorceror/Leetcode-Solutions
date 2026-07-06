114787777087bd9a5cb29a2e5bd8c0fbbed82d51
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-07-06 11:29:17 +0530

    Time: 157 ms (5.64%) | Memory: 61.5 MB (5.13%) - LeetSync

--- 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.cpp
+++ 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int MOD = 1e9 + 7;
 
-    long long rec(int n, int prev,vector<vector<int>> &dp){
+    long long rec(int n, int prev, vector<vector<int>>&dp){
         if(n==0) return 1;
 
         if(dp[n][prev] != -1) return dp[n][prev];
 
-        long long ways = 0;
-
-        ways = (ways + rec(n-1,0,dp)) % MOD;
+        long long notPlace = rec(n-1,0,dp) % MOD;
+        long long place = 0;
 
         if(prev == 0){
-            ways = (ways + rec(n-1,1,dp)) % MOD;
+            place = rec(n-1,1,dp) % MOD;
         }
-        return dp[n][prev] = ways;
+
+        return dp[n][prev] = (place + notPlace) % MOD;
     }
 
     int countHousePlacements(int n) {
-        vector<vector<int>> dp(n+1,vector<int>(2,-1));
-        long long one_side = rec(n,0,dp);
-        return (one_side*one_side) % MOD;
+        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        long long curr = rec(n,0,dp);
+        return (curr*curr) % MOD;
     }
 };
