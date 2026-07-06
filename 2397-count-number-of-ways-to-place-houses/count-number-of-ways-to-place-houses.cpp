89df1a5e701090a59318a07719e694e81d7037ce
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-07-06 11:34:54 +0530

    Time: 122 ms (8.72%) | Memory: 60.7 MB (7.18%) - LeetSync

--- 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.cpp
+++ 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.cpp
@@ -18,8 +18,16 @@ public:
     }
 
     int countHousePlacements(int n) {
-        vector<vector<int>>dp(n+1,vector<int>(2,-1));
-        long long curr = rec(n,0,dp);
-        return (curr*curr) % MOD;
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
+
+        dp[1][0] = 1;
+        dp[1][1] = 1;
+
+        for(int i=2; i<=n; i++){
+            dp[i][0] = (dp[i-1][0] + dp[i-1][1]) % MOD;
+            dp[i][1] = (dp[i-1][0]) % MOD;
+        }
+        long long ans = (dp[n][0] + dp[n][1]) % MOD;
+        return (ans*ans) % MOD;
     }
 };
