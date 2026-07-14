63545701e67e2f984c95b575df3a19fe4ef90240
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-07-14 17:47:41 +0530

    Time: 7 ms (86.63%) | Memory: 10.5 MB (80.79%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
+        vector<long long> dp(amount+1,0);
         const int LIMIT = INT_MAX;
-        vector<long long> dp(amount + 1, 0);
         dp[0] = 1;
-
-        for (int coin : coins) {
-            for (int i = coin; i <= amount; i++) {
-                dp[i] += dp[i - coin];
-                if (dp[i] > LIMIT) dp[i] = LIMIT;
+        for(int i=0; i<coins.size(); i++){
+            int wt = coins[i];
+            for(int j=wt; j<= amount; j++){
+                if (dp[j] > INT_MAX - dp[j - wt]) {
+                    dp[j] = INT_MAX;
+                } else {
+                    dp[j] = dp[j] + dp[j - wt];
+                }
             }
         }
         return (int)dp[amount];
