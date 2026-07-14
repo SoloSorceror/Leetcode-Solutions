ebed4d63d0893252efed84708974bfa8dfa67c5a
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-07-14 23:17:55 +0530

    Time: 3 ms (91.06%) | Memory: 10.3 MB (96.91%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -3,31 +3,23 @@ public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n, vector<int>(m,-1));
 
-        return rec(0,0,word1,word2, dp);
+        vector<int> dp(m+1,0);
+
+        for(int j =0; j<=m; j++){
+            dp[j] = j;
+        }
+
+        for(int i=1; i<=n; i++){
+            int diag = dp[0];
+            dp[0] = i;
+            for(int j=1; j<=m; j++){
+                int temp = dp[j];
+                if(word1[i-1] == word2[j-1]) dp[j] = diag;
+                else dp[j] = min({dp[j], dp[j-1], diag}) + 1;
+                diag = temp;
+            }
+        }
+        return dp[m];
     }
-
-private:
-    int rec(int i, int j,string word1, string word2,vector<vector<int>> &dp){
-        if(i==word1.size()){
-            return word2.size()-j;
-        }
-        if(j==word2.size()){
-            return word1.size()-i;
-        }
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(word1[i] == word2[j]){
-            return rec(i+1,j+1,word1,word2,dp);
-        }
-
-        int replace = rec(i+1,j+1,word1,word2,dp);
-        int del = rec(i+1,j,word1,word2,dp);
-        int insert = rec(i,j+1,word1,word2,dp);
-
-        return dp[i][j] = 1 + min({replace,del,insert});
-    }
-
 };
