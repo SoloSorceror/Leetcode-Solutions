ecbe419dbbcf06b8f8ca358650121b3e36ae58ac
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-07-15 21:02:50 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (84.15%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    void rec(int open, int close, int n, string str, vector<string> &ans){
-        if(str.size()==2*n){
-            ans.push_back(str);
+    vector<string> generateParenthesis(int n) {
+        string curr = "";
+        vector<string> ans;
+        rec(n,curr,0,0,ans);
+        return ans;
+    }
+
+private:
+    void rec(int n, string &curr, int openCount, int closeCount, vector<string> &ans){
+        if(curr.size() == 2*n){
+            ans.push_back(curr);
             return;
         }
-        if(open<n){
-            str.push_back('(');
-            rec(open+1,close,n,str,ans);
-            str.pop_back();
+        if(openCount < n){
+            curr.push_back('(');
+            rec(n,curr,openCount+1,closeCount,ans);
+            curr.pop_back();
         }
-        if(close<open){
-            str.push_back(')');
-            rec(open,close+1,n,str,ans);
-            str.pop_back();
+        if (closeCount < openCount){
+            curr.push_back(')');
+            rec(n,curr,openCount,closeCount+1,ans);
+            curr.pop_back();
         }
-    }
-    vector<string> generateParenthesis(int n) {
-        string str = "";
-        int open = 0, close = 0;
-        vector<string> ans;
-        rec(open,close,n,str,ans);
-        return ans;
     }
 };
