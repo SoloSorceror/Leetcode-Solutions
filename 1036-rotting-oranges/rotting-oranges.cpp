a3ddfdb8cf29c0168b3e3e8195a9da2296ce1efc
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-07-20 22:10:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (29.06%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -4,46 +4,48 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        queue<pair<int,int>> q;
         int fresh = 0;
+        vector<vector<int>> visited(n,vector<int>(m,0));
+        queue<pair<int,int>> q;
 
-        // Step 1: Push all rotten oranges
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 2)
-                    q.push({i, j});
-                else if(grid[i][j] == 1)
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == 2){
+                    visited[i][j] = 2;
+                    q.push({i,j});
+                }
+                else if(grid[i][j] == 1){
+                    visited[i][j] = 1;
                     fresh++;
-            }
-        }
-
-        int dx[4] = {-1, 0, 1, 0};
-        int dy[4] = {0, 1, 0, -1};
-
-        int time = 0;
-
-        // Step 2: Multi-source BFS
-        while(!q.empty() && fresh > 0){
-            int sz = q.size();
-            time++;
-
-            for(int i = 0; i < sz; i++){
-                auto [r, c] = q.front();
-                q.pop();
-
-                for(int d = 0; d < 4; d++){
-                    int nr = r + dx[d];
-                    int nc = c + dy[d];
-
-                    if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1){
-                        grid[nr][nc] = 2;   // mark rotten
-                        fresh--;
-                        q.push({nr, nc});
-                    }
                 }
             }
         }
 
+        int dx[4] = {-1,0,1,0};
+        int dy[4] = {0,1,0,-1};
+
+        int time = 0;
+
+        while(!q.empty() && fresh>0){
+            int sz = q.size();
+            time++;
+            while(sz--){
+                auto [row,col] = q.front();
+                q.pop();
+
+                for(int i=0; i<4; i++){
+                    int nr = row + dx[i];
+                    int nc = col + dy[i];
+
+                    if(nr>=0 && nc>=0 && nr<n && nc<m && visited[nr][nc] == 1){
+                        q.push({nr,nc});
+                        visited[nr][nc] = 2;
+                        fresh--;
+                    }
+                }
+
+            }
+        }
         return fresh == 0 ? time : -1;
     }
 };
