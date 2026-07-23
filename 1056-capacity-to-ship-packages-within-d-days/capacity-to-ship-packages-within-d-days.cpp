4d6bbb73b0ed8d0f493b56df9ecdec4894dc8979
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-07-23 23:05:41 +0530

    Time: 11 ms (65.24%) | Memory: 35.1 MB (19.69%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    bool canShip(int mid,vector<int>& weights, int days){
-        int count = 1;
+    int canShip(vector<int>& weights, int days, int mid){
+        int time = 1;
         int sum = 0;
-        for(auto &a: weights){
-            if(sum+a>mid){
-                sum = a;
-                count++;
+        for(auto &w: weights){
+            if(sum + w > mid){
+                time++;
+                sum = w;
             }
-            else{
-                sum +=a;
-            }
+            else sum += w;
         }
-        return count<=days;
+        return time <= days;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        int low = *max_element(weights.begin(),weights.end());
-        int high = accumulate(weights.begin(),weights.end(),0);
+        int n = weights.size();
+        int left = *max_element(weights.begin(),weights.end());
+        int right = accumulate(weights.begin(),weights.end(),0);
+        int ans = 0;
 
-        int ans = high;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(canShip(mid,weights,days)){
-                high = mid-1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(canShip(weights,days,mid)){
                 ans = mid;
-            }
-            else{
-                low = mid+1;
+                right = mid -1;
+            }else{
+                left = mid+1;
             }
         }
         return ans;
