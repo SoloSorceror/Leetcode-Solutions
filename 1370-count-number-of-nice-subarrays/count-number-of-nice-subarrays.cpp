cb2d1099a81710f5268b7b5f2532cddb943c849e
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-09-11 12:38:52 +0530

    Time: 13 ms (34.14%) | Memory: 74.7 MB (28.30%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int goal) {
-        if (goal < 0)
-            return 0;
+    int helper(vector<int> &nums, int goal){
+        if(goal<0) return 0;
+
         int left = 0;
+        int curr = 0;
         int count = 0;
-        int sum = 0;
-        for (int right = 0; right < nums.size(); right++) {
-            sum += nums[right];
-            while (sum > goal) {
-                sum -= nums[left];
+
+        for(int right =0; right<nums.size(); right++){
+            curr += nums[right];
+
+            while(curr > goal){
+                curr -= nums[left];
                 left++;
             }
-            count += right - left + 1;
+            count += right-left+1;
         }
         return count;
     }
 
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] % 2 == 0) {
-                nums[i] = 0;
-            } else {
-                nums[i] = 1;
-            }
+    int numberOfSubarrays(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> nums(n,0);
+
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i]%2 == 0) nums[i] = 0;
+            else nums[i] = 1;
         }
-        return helper(nums, k) - helper(nums, k - 1);
+        return helper(nums,k) - helper(nums,k-1);
     }
 };
