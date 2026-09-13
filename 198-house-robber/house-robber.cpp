081ed68f4559c5f6b08cc5560690ee8410d0818f
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-09-13 23:42:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (46.37%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -6,15 +6,16 @@ public:
 
         if(n==1) return nums[0];
 
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        int prev2 = nums[0];
+        int prev1 = max(nums[0], nums[1]);
 
         for(int i=2; i<n; i++){
-            int notPick = dp[i-1];
-            int pick = nums[i] + dp[i-2];
-            dp[i] = max(pick,notPick);
+            int notPick = prev1;
+            int pick = nums[i] + prev2;
+            prev2 = prev1;
+            prev1 = max(pick,notPick);
         }
 
-        return dp[n-1];
+        return prev1;
     }
 };
