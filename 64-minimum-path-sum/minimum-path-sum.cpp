287d6d092c7a797ae5ddf3883732c42263c22275
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-09-15 00:05:40 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (89.24%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -13,15 +13,15 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        vector<vector<int>> dp(n+1,vector<int>(m+1,1e9));
+        vector<int> dp(m+1,1e9);
 
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
-                if(i==1 && j==1) dp[1][1] = grid[0][0]; 
-                else dp[i][j] = grid[i-1][j-1] + min(dp[i][j-1], dp[i-1][j]);
+                if(i==1 && j==1) dp[1] = grid[0][0]; 
+                else dp[j] = grid[i-1][j-1] + min(dp[j-1], dp[j]);
             }
         }
 
-        return dp[n][m];
+        return dp[m];
     }
 };
