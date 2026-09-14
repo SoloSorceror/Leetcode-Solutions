3e45de5ed752819b18e95bfb1455fd49caadcdd7
Sanjay Chetry <146775376+SoloSorceror@users.noreply.github.com>
2026-09-15 00:08:05 +0530

    Time: 4 ms (14.95%) | Memory: 15.5 MB (79.28%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    int rec(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp){
-        if(i==0 && j==0) return grid[i][j];
-        if(i<0 || j< 0) return 1e9;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        return dp[i][j] = grid[i][j] + min(rec(i-1,j,grid,dp), rec(i,j-1,grid,dp));
-    }
-
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-
+        
         vector<int> dp(m+1,1e9);
+        dp[1] = 0;
 
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
-                if(i==1 && j==1) dp[1] = grid[0][0]; 
-                else dp[j] = grid[i-1][j-1] + min(dp[j-1], dp[j]);
+                dp[j] = grid[i-1][j-1] + min(dp[j-1], dp[j]);
             }
         }
-
         return dp[m];
     }
 };
